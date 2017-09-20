ca54b904ddc4e0d082766bd855f10eb21f690929
sarahc123 <g00330821@gmit.ie>
2017-09-20 17:57:59 +0100

    current time working

---
+++ 02-Current-Time.go
@@ -0,0 +1,17 @@
+//Author Sarah Carroll
+//Date :20/09/2017
+
+// Adapter from:https://tour.golang.org/welcome/4
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcome to the playground!")
+
+	fmt.Println("The time is", time.Now())
+}
