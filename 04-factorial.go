c88407fe54b6fd8adb4e5a7548b1ccb440a8f5ba
sarahc123 <g00330821@gmit.ie>
2017-09-22 17:09:44 +0100

    added comments

--- 04-factorial.go
+++ 04-factorial.go
@@ -1,8 +1,11 @@
+//Author Sarah Carroll
+//Date :21/09/2017
+
 package main
 
 import "fmt"
 
-func factorial(x uint64) uint64 {
+func factorial(x uint64) uint64 { //uint64 works for large integers
 	if x == 0 {
 		return 1
 	}
@@ -22,6 +25,7 @@ func main() {
 	
 	total = 0
 	
+	//for loop doesnt need ()
 	for calcFactorial > 0 {
 		rem = calcFactorial % 10
 		total = total + rem
