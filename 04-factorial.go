e560f7344fa67ba5d605ccdac249d54390e58283
sarahc123 <g00330821@gmit.ie>
2017-09-22 17:03:32 +0100

    factorial,where not too large

---
+++ 04-factorial.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func factorial(x uint64) uint64 {
+	if x == 0 {
+		return 1
+	}
+	
+	return x * factorial(x-1)
+}
+
+func main() {
+	var calcFactorial uint64
+	var total uint64
+	var rem uint64
+	
+	x := uint64(15)
+	
+	calcFactorial = factorial(x)
+	fmt.Println("The Factorial of", x, " = ", calcFactorial)
+	
+	total = 0
+	
+	for calcFactorial > 0 {
+		rem = calcFactorial % 10
+		total = total + rem
+		calcFactorial = calcFactorial / 10
+	}
+	
+	fmt.Println("The Sum of the Factorial of", x, "=", total)
+	
+}
