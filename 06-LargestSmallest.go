57a630dc1b0816474bcc5bdb35a907080920a5c5
sarahc123 <g00330821@gmit.ie>
2017-09-27 17:26:05 +0100

    random list of numbers between 0-100

--- 06-LargestSmallest.go
+++ 06-LargestSmallest.go
@@ -3,29 +3,40 @@
 // Adapter from:https://gist.github.com/pyk/10519339
 // GO Program to print out Large and Smallest Values in a List.
 
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 
 	//variables
 	var i, n, large, small int
-	
+	var nonPrimes [10]int
+	rand.Seed(time.Now().UnixNano())
+
+	for i = 0; i < 10; i++ {
+		nonPrimes[i] = (rand.Intn(100) + 1)
+	}
+	/*for i = 0; i < 10; i++ {
+		fmt.Print(nonPrimes[i], ",")
+	}*/
 	//list in the form of an array
-	nonPrimes := [8]int{9,12,6,14,1,10,4,8}
-	
-	fmt.Println("List =",nonPrimes)
-	
+	//nonPrimes := [8]int{9,12,6,14,1,10,4,8}
+
+	fmt.Println("List =", nonPrimes)
+
 	n = len(nonPrimes)
-	
+
 	fmt.Println("Length of List =", n)
-	
+
 	//set smallest and largest
 	small = 999999
 	large = -1
-	
+
 	//for loop to compare list with set smallest and largest
 	for i = 0; i < n; i++ {
 
@@ -36,11 +47,12 @@ func main() {
 		if nonPrimes[i] > large {
 			large = nonPrimes[i]
 		}
-		
+
 	}
-	
+
 	//print out results
-	fmt.Println("Smallest Value =",small)
-	fmt.Println("Largest  Value =",large)
-	
+
+	fmt.Println("Smallest Value =", small)
+	fmt.Println("Largest  Value =", large)
+
 }
