e9e685d05e49ee4365d26ad927f90f54222fa6fd
sarahc123 <g00330821@gmit.ie>
2017-09-29 11:42:10 +0100

    formated time alone in hours and mins

--- 02-Current-Time.go
+++ 02-Current-Time.go
@@ -1,7 +1,7 @@
 //Author Sarah Carroll
 //Date :20/09/2017
 
-// Adapter from:https://tour.golang.org/welcome/4 https://golang.org/pkg/time/
+// Adapter from:https://tour.golang.org/welcome/4 https://golang.org/pkg/time/ https://stackoverflow.com/questions/5885486/how-do-i-get-the-current-timestamp-in-go
 
 package main
 
@@ -12,7 +12,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Welcome to the playground!")
+	t := time.Now()
 
-	fmt.Println("The time is", time.Now())
+	//current time date printed in default format
+	fmt.Println("The current date and time is", time.Now())
+
+	//format time currently,in hours and mins
+	fmt.Println(t.Format("\n15.04"))
 }
