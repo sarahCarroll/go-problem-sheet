1844b24d2623072e4b820afd03651c51085d892b
sarahc123 <g00330821@gmit.ie>
2017-10-02 18:30:13 +0100

    This version pre-sorts two unsorted int arrays (lists) and then merges them!

--- 08-Merge_Sort.go
+++ 08-Merge_Sort.go
@@ -3,59 +3,78 @@
 //
 // GO Program to Merge 2 Sorted Lists.
 
+
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"sort"
+)
 
 func main() {
 
 	var i, x, y int
-
-	primes := [6]int{2, 3, 5, 7, 11, 13}
-	nonPrimes := [8]int{1, 4, 6, 8, 9, 10, 12, 14}
-	merged := [14]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
-	fmt.Println("primes    =", primes)
-	fmt.Println("nonPrimes =", nonPrimes)
-	fmt.Println("merged    =", merged)
-
+	
+	primes    := []int{13,11,7,5,3,2}			// unsorted
+	nonPrimes := []int{14,12,10,9,8,6,4,1}		// unsorted
+	
+	merged    := [14]int{0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	
+	fmt.Println("Lists at Start")
+	fmt.Println("--------------")
+	
+	fmt.Println("primes    =",primes)
+	fmt.Println("nonPrimes =",nonPrimes)
+	fmt.Println("merged    =",merged)
+	
 	p := len(primes)
 	n := len(nonPrimes)
 	m := len(merged)
-
+	
 	fmt.Println("Length of primes    =", p)
 	fmt.Println("Length of nonPrimes =", n)
 	fmt.Println("Length of merged    =", m)
-
-	i = 0 // merged index
-	x = 0 // primes index
-	y = 0 // non-primes index
-
-	for x < p && y < n {
-
-		if primes[x] < nonPrimes[y] {
+	
+	// Sort the 2 arrays before merging
+	
+	sort.Ints(primes)
+	sort.Ints(nonPrimes)
+	
+	i = 0	// merged index
+	x = 0	// primes index
+	y = 0	// non-primes index
+	
+	// Merge lists in ascending int order
+	
+	for ( x < p && y < n ) {
+	
+		if( primes[x] < nonPrimes[y]) {
 			merged[i] = primes[x]
 			x++
 		} else {
 			merged[i] = nonPrimes[y]
 			y++
 		}
-
+		
 		i++
-
+		
 	}
-
-	for x < p {
+	
+	for ( x < p) {
 		merged[i] = primes[x]
 		i++
 		x++
 	}
 
-	for y < n {
+	for ( y < n) {
 		merged[i] = nonPrimes[y]
 		i++
 		y++
 	}
-
-	fmt.Println("New merged=", merged)
+	
+	fmt.Println("Lists at Finish")
+	fmt.Println("---------------")
+	fmt.Println("primes    =",primes)
+	fmt.Println("nonPrimes =",nonPrimes)
+	fmt.Println("New merged=",merged)
 }
